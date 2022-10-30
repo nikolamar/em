dfe27bd3be21ce2656f6d927d2baa34ed9f4fe5c
Nikola Marjanovic <nikolamar@gmail.com>
2022-10-30 15:29:21 +0100

    feat: refactor em store to use pub sub

--- src/core.ts
+++ src/core.ts
@@ -1,312 +1,179 @@
 import React from "react";
-import produce, { enableMapSet as enableImmerMapSet } from "immer";
-import type { EM, Callback, ProviderProps, CompWithStateProps } from "./types";
+import produce, { enableMapSet } from "immer";
+// types
+import type { EMStoreProps, Producer } from "./types";
 
-const em: EM = {
-  consoleLog: false,
-  persistent: false,
-  enableMapSet: false,
-  performanceLog: false,
-};
-
-let states: Map<string, any>;
-let setRef: () => void;
-
-const EventContext = React.createContext({} as any);
+// constants
+const PREFIX = "EMStore";
 
 /**
- * Get by key an array with state and callback function
- * @param {string} key - Get by key an array with state and callback function
- * @param {string} name - Event name
+ * Creates a new store and returns a react provider and a hook function and an event state function
+ * @param {object} key - object with multiple states
+ * @param {object} options - object to configure the store
  */
-export function state<T = any>(key: string, name?: string) {
-  const value = states?.get(key);
-  return [
-    value,
-    (callback: Callback) => {
-      const immValue = produce(value, callback);
-      states?.set(key, immValue);
-
-      if (em.consoleLog) {
-        console.log(
-          `%c${new Date()
-            .toTimeString()
-            .substr(
-              0,
-              8
-            )} %cevent: %c${name} %cstate: %c${key} %cvalue: %c${handleValue(
-            value
-          )} %cnewvalue: %c${handleValue(immValue)}`,
-          "color: black",
-          "color: grey",
-          "color: black",
-          "color: grey",
-          "color: black",
-          "color: grey",
-          "color: black",
-          "color: grey",
-          "color: black"
-        );
-      }
-
-      /**
-       * Prevents multiple renders of same value
-       */
-      if (immValue === value) return;
-
-      setRef();
-    },
-  ];
-}
-
-let perfEnd = 0;
-let perfStart = 0;
-let perfTime = 0;
-let perfAccTime = 0;
-let perfTimeout = 0;
-let timeoutStore = 0;
-let timeoutState = 0;
-
-/**
- * ReactJS HOC — injects requested states from the second argument
- * @param {Object} component - React Component
- * @param {Array.<string>} keys - Get an array of states in prop by adding a second argument array of keys
- */
-export function withState<T = any>(component: React.FC<T>, keys: string[]) {
-  return ({ children, ...rest }: CompWithStateProps) => {
-    /**
-     * This is rendered every single time since it is used with context.
-     * I could just ditch context and rerender the component manually.
-     */
-    if (em.performanceLog) {
-      perfStart = performance.now();
-    }
-
-    React.useContext(EventContext);
-    const dependency = keys.map((key) => states.get(key));
-
-    if (em.performanceLog) {
-      perfEnd = performance.now();
-      perfTime = perfEnd - perfStart;
-      perfAccTime += perfTime;
-
-      // log each performance measure
-      console.log(
-        `%c${new Date()
-          .toTimeString()
-          .substr(0, 8)} %ctime: %c${perfTime}ms %cstates: %c${keys}`,
-        "color: black",
-        "color: grey",
-        "color: black",
-        "color: grey",
-        "color: black"
-      );
-
-      cancelAnimationFrame(perfTimeout);
-      perfTimeout = requestAnimationFrame(() => {
-        // log accumulate performance measure
-        console.log(
-          `%c${new Date()
-            .toTimeString()
-            .substr(0, 8)} %ccumulative time: %c${perfAccTime}ms`,
-          "color: black",
-          "color: grey",
-          "color: black"
-        );
-
-        // clear accumulate performance measure
-        perfAccTime = 0;
-      });
-    }
-
-    return React.useMemo(() => {
-      /**
-       * React.useMemo() compares the states.
-       * This is rendered only if the state is changed.
-       */
-      return React.createElement(
-        component as any,
-        { ...rest, states: dependency },
-        children
-      );
-    }, dependency);
-  };
-}
-
-/**
- * Context Provider for states
- * @param {Map<String, *>} value - initial states
- * @param {*} children - react components
- * @param {callback} onChange - callback function when state is changed
- * @param {boolean} consoleLog - enable console.log state changes
- * @param {boolean} persistent - enable persistent states
- * @param {boolean} enableMapSet - boolean to enable Map and Set in immer
- * @param {boolean} performanceLog - enable performance log of states changes
- * @param {callback} serializeStates - serialize states to string
- * @param {callback} deserializeStates -deserialize states from string
- */
-export function Provider({
-  value,
-  children,
-  onChange,
-  consoleLog = false,
-  persistent = false,
-  enableMapSet = false,
-  performanceLog = false,
-  serializeStates = null,
-  deserializeStates = null,
-}: ProviderProps) {
-  const [state, setState] = React.useState(0);
-  const [ready, setReady] = React.useState(false);
-
-  /**
-   * React useCallback() set function re-render components which is used with context
-   * and call onChange callback function
-   */
-  const saveState = React.useCallback(() => {
-    try {
-      if (onChange) {
-        if (typeof onChange !== "function") {
-          throw new Error("onChange prop in Provider is not a function");
-        }
-
-        cancelAnimationFrame(timeoutState);
-        timeoutState = requestAnimationFrame(() => {
-          onChange(states);
-        });
-      }
-
-      if (!persistent) {
-        return;
-      }
-
-      cancelAnimationFrame(timeoutStore);
-      timeoutStore = requestAnimationFrame(() => {
-        if (!states?.entries) {
-          throw new Error("value prop should be a Map object");
-        }
-
-        if (serializeStates) {
-          if (typeof serializeStates !== "function") {
-            throw new Error("serializeStates prop is not a function");
-          }
-
-          const result = serializeStates(states);
-          localStorage.setItem("emstate", JSON.stringify(result));
-          return;
-        }
-
-        localStorage.setItem(
-          "emstates",
-          JSON.stringify(Array.from(states.entries()))
-        );
-      });
-    } catch (e) {
-      console.log("error saving states", e);
-    }
-  }, [onChange, persistent, serializeStates]);
-
-  /**
-   *  React useEffetct() to initialize states
-   */
-  React.useEffect(() => {
-    states = value;
-    setReady(true);
-  }, [value]);
-
-  /**
-   * React useEffetct() used to enable immer Map and Set
-   */
-  React.useEffect(() => {
-    if (enableMapSet) enableImmerMapSet();
-  }, [enableMapSet]);
-
-  /**
-   * React useEffetct() used to set setRef function
-   */
-  React.useEffect(() => {
-    setRef = () => {
-      setState(state + 1);
-      saveState();
-    };
-  }, [state, setState, saveState]);
-
-  /**
-   * React useEffetct() used to restore states from localStorage
-   */
-  React.useEffect(() => {
-    if (!persistent) {
-      return;
-    }
-
-    try {
-      const emstateString = localStorage.getItem("emstate");
-
-      if (!emstateString) {
-        return;
-      }
-
-      /**
-       * Deserialize states from string if deserializeStates is provided and set states
-       * NOTE: this will work event if states are not serializable since user is overriding defaults
-       */
-      if (deserializeStates) {
-        if (typeof deserializeStates !== "function") {
-          throw new Error("deserializeStates prop is not a function");
-        }
-
-        const result = deserializeStates(emstateString);
-        states = new Map<string, any>(result);
-        return setState(state + 1);
-      }
-
-      /**
-       * If deserializeStates is not provided, set states from localStorage
-       * NOTE: this will not work if the states are not serializable
-       */
-      const emParsedStates = JSON.parse(emstateString);
-
-      if (typeof emParsedStates !== "object") {
-        throw new Error("parsed state is not a object");
-      }
-
-      states = new Map<string, any>(emParsedStates);
-      setState(state + 1);
-    } catch (e) {
-      console.log("error loading state from storage", e);
-    }
-  }, [deserializeStates, persistent]);
-
-  /**
-   * React useEffect() used to set config for emstore
-   */
-  React.useEffect(() => {
-    em.consoleLog = consoleLog;
-    em.persistent = persistent;
-    em.enableMapSet = enableMapSet;
-    em.performanceLog = performanceLog;
-  }, [performanceLog, consoleLog, persistent, enableMapSet]);
-
-  if (!ready) {
-    return null;
+export function createStore<T>(states: T, props: EMStoreProps = null) {
+  if (props?.enableMapAndSet) {
+    enableMapSet();
   }
 
-  return React.createElement(EventContext.Provider, { value: state }, children);
+  if (props?.persist) {
+    if (typeof props?.handleLocalStorageDataLoad === "function") {
+      props.handleLocalStorageDataLoad(states as Record<string, any>);
+    } else {
+      loadStatesFromLocalStorage(states as Record<string, any>);
+    }
+  }
+
+  const subscribers = new Set<() => void>();
+
+  const subscribe = (callback: () => void) => {
+    subscribers.add(callback);
+    return () => subscribers.delete(callback);
+  };
+
+  const context = React.createContext(states);
+
+  function Provider({ children }: { children: React.ReactNode }) {
+    return React.createElement(context.Provider, { value: states }, children);
+  }
+
+  const set = <K extends keyof T>(
+    key: K,
+    producer: Producer,
+    event: string
+  ) => {
+    const prevState = states[key];
+    const nextState = produce(prevState, producer);
+
+    if (props?.consoleLog) {
+      consoleLogChanges(key as string, event, prevState, nextState);
+    }
+
+    if (props?.persist) {
+      if (typeof props?.handleLocalStorageDataSave === "function") {
+        props.handleLocalStorageDataSave(key as string, prevState, nextState);
+      } else {
+        saveStateToLocalStorage(key as string, nextState);
+      }
+    }
+
+    states[key] = nextState;
+
+    if (typeof props?.onSetState === "function") {
+      props.onSetState(key as string, prevState, nextState);
+    }
+
+    subscribers.forEach((callback) => callback());
+  };
+
+  const state = <K extends keyof T>(
+    key: K,
+    event = "event"
+  ): [T[K], Producer] => {
+    return [states[key], (producer: Producer) => set(key, producer, event)];
+  };
+
+  const eventState =
+    (event: string) =>
+    <K extends keyof T>(key: K) =>
+      state(key, event);
+
+  const useState = <K extends keyof T>(key: K): [T[K], Producer] => {
+    React.useSyncExternalStore(subscribe, () => states[key]);
+    return state(key);
+  };
+
+  return { Provider, useState, eventState };
 }
 
 /**
- * Function to handle console.log and different types of values
- * @param {*} value - Value to be logged
+ * Save state to local storage
+ * @param {string} key - object key of state
+ * @param {*} nextState - next state value to be console logged
  */
-function handleValue(value: any) {
-  if (typeof value === "string" && value !== "") return `"${value}"`;
-  else if (value === "") return `""`;
-  else if (typeof value === "boolean") return value;
-  else if (value === null) return null;
-  else if (value === undefined) return undefined;
-  else if (typeof value === "object" && value.get)
-    return JSON.stringify(Object.fromEntries(value), null, 2);
-  else if (typeof value === "object") return JSON.stringify(value, null, 2);
-  else if (typeof value === "number") return value;
-  else return value;
+function saveStateToLocalStorage(key: string, nextState: any) {
+  try {
+    localStorage.setItem(`${PREFIX}.${key}`, JSON.stringify(nextState));
+  } catch (e) {
+    console.log("writing local storage error", e);
+  }
+}
+
+/**
+ * Load state from local storage
+ */
+function loadStatesFromLocalStorage(states: Record<string, any>) {
+  try {
+    Object.keys(localStorage).forEach((localStorageKey: string) => {
+      if (localStorageKey.includes(PREFIX)) {
+        // parse state
+        const json: any = localStorage.getItem(localStorageKey);
+        const parsedState: any = JSON.parse(json);
+
+        // cleanup prefix EMStore
+        const stateKey: any = localStorageKey.replace(`${PREFIX}.`, "");
+
+        // update states object
+        Object.keys(states).forEach((stateKeyInStates: string) => {
+          if (stateKeyInStates === stateKey) {
+            states[stateKeyInStates] = parsedState;
+          }
+        });
+      }
+    });
+  } catch (e) {
+    console.log("loading local storage error", e);
+  }
+}
+
+/**
+ * Handle different types of state values and return a string
+ * @param {*} nextState - nest state value to be console logged
+ */
+function handleValue(nextState: any) {
+  if (typeof nextState === "string" && nextState !== "")
+    return `"${nextState}"`;
+  else if (nextState === "") return `""`;
+  else if (typeof nextState === "boolean") return nextState;
+  else if (nextState === null) return null;
+  else if (nextState === undefined) return undefined;
+  else if (typeof nextState === "object" && nextState?.get)
+    return JSON.stringify(Object.fromEntries(nextState), null, 2);
+  else if (typeof nextState === "object")
+    return JSON.stringify(nextState, null, 2);
+  else if (typeof nextState === "number") return nextState;
+  else return nextState;
+}
+
+/**
+ * Logs state change and logs them to console
+ * @param {string} key - object key of state
+ * @param {string} event - event name
+ * @param {*} prevState - previous state value
+ * @param {*} nextState - next state value
+ */
+function consoleLogChanges(
+  key: string,
+  event: string,
+  prevState: any,
+  nextState: any
+) {
+  console.log(
+    `%c${new Date()
+      .toTimeString()
+      .substr(0, 8)} %cevent: %c${event} %cstate: %c${
+      key as string
+    } %cvalue: %c${handleValue(prevState)} %cnewvalue: %c${handleValue(
+      nextState
+    )}`,
+    "color: black",
+    "color: grey",
+    "color: black",
+    "color: grey",
+    "color: black",
+    "color: grey",
+    "color: black",
+    "color: grey",
+    "color: black"
+  );
 }
